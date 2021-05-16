fe32115dcc52d82d07a01bc41b74e24481bdf99e
gokulrajanpillai <gokulrajanpillai@gmail.com>
2021-05-16 16:43:16 +0200

    Initialised git project

---
+++ metrics_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  metrics_iOS
+//
+//  Created by Gokul Rajan on 16.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ metrics_iOS/metrics_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  metrics_iOSApp.swift
+//  metrics_iOS
+//
+//  Created by Gokul Rajan on 16.05.21.
+//
+
+import SwiftUI
+
+@main
+struct metrics_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
